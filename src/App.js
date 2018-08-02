c9db1a8bcadd69405f2a1b1bba506ee7425b85a1
Dawid Walenciej <dawid101w@gmail.com>
2018-08-02 19:00:29 +0200

    added tensorflow.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Tensorflow from './components/Tensorflow/Tensorflow';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Tensorflow />
       </div>
     );
   }
---
+++ src/components/Tensorflow/Tensorflow.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import * as tf from '@tensorflow/tfjs';
+
+class Tensorflow extends Component {
+  render() {
+    return(
+      <div>
+        <p>Tensorflow Graph SOMEDAY...</p>
+      </div>
+    );
+  }
+}
+
+export default Tensorflow;
