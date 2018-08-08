4277698cac2ffe07184ddf85fccd5e1d49fd3600
Dawid <dawid101w@gmail.com>
2018-08-08 13:06:24 +0200

    hotfix working with separate server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
