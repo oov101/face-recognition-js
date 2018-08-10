eb567ce266f875ad3d37b514da0c35a76b656d4d
Dawid Walenciej <dawid101w@gmail.com>
2018-08-10 20:15:11 +0200

    change posistions

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -4,9 +4,9 @@ class Welcome extends Component {
   render() {
     return(
       <div>
-        <h1>Face Recognition Application</h1>
         <Link to="/Tutorial"><button>Tutorial</button></Link>
         <Link to="/FaceReco"><button>Start</button></Link>
+        <h1>Face Recognition Application</h1>
       </div>
     );
   }
