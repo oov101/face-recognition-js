a23d7ddde3de2c7673cf36d32495175b9987e2da
Dawid Walenciej <dawid101w@gmail.com>
2018-08-10 23:18:47 +0200

    add styles global, and Welcome Component

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './Welcome.css';
+
 class Welcome extends Component {
   render() {
     return(
-      <div>
-        <Link to="/Tutorial"><button>Tutorial</button></Link>
-        <Link to="/FaceReco"><button>Start</button></Link>
-        <h1>Face Recognition Application</h1>
+      <div id="Welcome">
+        <div>
+          <h1>Face Recognition Application</h1>
+          <Link to="/Tutorial"><button>Tutorial</button></Link>
+          <Link to="/FaceReco"><button>Start</button></Link>
+        </div>
       </div>
     );
   }
