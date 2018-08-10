46d4cec2fc0eeaf72fadad6d5b52d769a7e9f75d
Dawid Walenciej <dawid101w@gmail.com>
2018-08-10 19:46:36 +0200

    home button added to Tutorial Component

--- src/components/Tutorial/Tutorial.js
+++ src/components/Tutorial/Tutorial.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Tutorial extends Component {
   render() {
     return(
       <div>
+        <Link to="/"><button>Home</button></Link>
         <h1>Tutorial</h1>
       </div>
     );
