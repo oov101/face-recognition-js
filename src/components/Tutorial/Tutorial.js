fb5fd30d110693ce990bfee6b51bab2c27ddffd5
Dawid Walenciej <dawid101w@gmail.com>
2018-08-10 19:37:35 +0200

    Tutorial Component added

---
+++ src/components/Tutorial/Tutorial.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Tutorial extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Tutorial</h1>
+      </div>
+    );
+  }
+}
+
+export default Tutorial;
