e046982bcb2532a44c90d7e2b72e58d09bf8a19c
Dawid Walenciej <dawid101w@gmail.com>
2018-08-10 18:24:35 +0200

    routing added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import FaceApi from './components/FaceApi/FaceApi';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 class App extends Component {
   
   render() {
     return (
-      <div className="App">
-        <FaceApi />
-      </div>
+      <Router>
+        <div className="App">
+          <Welcome exact path="/" component={Welcome} />
+          <FaceApi path="/FaceReco" />
+        </div>
+      </Router>
     );
   }
 }
