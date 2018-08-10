ee6e7067b40274634009b2b5d56e1ae8f6139d40
Dawid Walenciej <dawid101w@gmail.com>
2018-08-10 19:19:07 +0200

    Import Link from react-router-dom, btn link to FaceReco

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 class Welcome extends Component {
   render() {
     return(
       <div>
-        <p>Welcome</p>
+        <h1>Face Recognition Application</h1>
+        <button>Tutorial</button>
+        <Link to="/FaceReco"><button>Start</button></Link>
       </div>
     );
   }
