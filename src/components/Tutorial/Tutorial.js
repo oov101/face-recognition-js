b1d4139976d967ab4645d726322920dd347d175a
Dawid <dawid101w@gmail.com>
2018-08-13 13:11:34 +0200

    Tutorial styles

--- src/components/Tutorial/Tutorial.js
+++ src/components/Tutorial/Tutorial.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './Tutorial.css'
 
 class Tutorial extends Component {
   render() {
     return(
-      <div>
+      <div id="Tutorial">
         <Link to="/"><button>Home</button></Link>
         <h1>Tutorial</h1>
       </div>
