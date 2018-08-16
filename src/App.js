c04372dbb9cb767362cc13cb9cddc60efa042ce6
Dawid Walenciej <dawid101w@gmail.com>
2018-08-16 20:58:56 +0200

    routing transitions added!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, withRouter, Switch } from 'react-router-dom';
 import './App.css';
 import FaceReco from './components/FaceReco/FaceReco';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Welcome from './components/Welcome/Welcome';
 import Tutorial from './components/Tutorial/Tutorial';
 import WindowButtons from './components/WindowButtons/WindowButtons';
 
 class App extends Component {
   render() {
-    return (
+    return(
       <Router>
-        <div className="App">
+        <div>
           <WindowButtons />
-          <Route exact path="/" component={Welcome} />
-          <Route path="/FaceReco" component={FaceReco} />
-          <Route path="/Tutorial" component={Tutorial} />
+          <RouterTransition />
         </div>
       </Router>
     );
   }
 }
+const RouterTransition = withRouter(({ location }) =>
+  <div className="App">
+    <TransitionGroup>
+      <CSSTransition
+        key={location.key}
+        classNames='fade'
+        timeout={300}
+      >
+        <Switch location={location}>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/FaceReco" component={FaceReco} />
+          <Route path="/Tutorial" component={Tutorial} />
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
+  </div>
+);
+
+
 
 export default App;
