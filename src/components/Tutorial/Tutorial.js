2b0c89db6e7e422c784f707b2018f7deb94b013f
Dawid <dawid101w@gmail.com>
2018-08-18 12:01:13 +0200

    switching between tips implemented

--- src/components/Tutorial/Tutorial.js
+++ src/components/Tutorial/Tutorial.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Tutorial.css'
+import tutorialTips from './tutorial-tips';
+import tutorial1 from '../../assets/tutorial_images/1.png';
+import tutorial2 from '../../assets/tutorial_images/2.png';
+import tutorial3 from '../../assets/tutorial_images/3.png';
+import tutorial4 from '../../assets/tutorial_images/4.png';
+import tutorial5 from '../../assets/tutorial_images/5.png';
+
+const tutorialImages = [tutorial1, tutorial2, tutorial3, tutorial4, tutorial5];
 
 class Tutorial extends Component {
   render() {
@@ -14,28 +22,65 @@ class Tutorial extends Component {
   }
 }
 
-const TutorialContainer = () => {
-  return (
-    <div>
-      <NavigationButton />
-      <TutorialImageContainer />
-      <NavigationButton />
-    </div>
-  );
+class TutorialContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      indexOfTip: 0,
+    }
+
+    this.prevTip = this.prevTip.bind(this);
+    this.nextTip  = this.nextTip.bind(this);
+  }
+
+  prevTip () {
+    
+    if(this.state.indexOfTip === 0) {
+      return;
+    }
+    console.log('prev', this.state.indexOfTip);
+
+    this.setState({
+      indexOfTip: --(this.state.indexOfTip)
+    });
+  }
+
+  nextTip() {
+    
+    if(this.state.indexOfTip === 4) {
+      return;
+    }
+    console.log('next', this.state.indexOfTip);
+
+    this.setState({
+      indexOfTip: ++(this.state.indexOfTip)
+    })
+  }
+
+  render() {
+    return (
+      <div className="TutorialContainer" >
+        <NavigationButton direction={"prev"} update={this.prevTip} />
+        <TutorialImageContainer indexOfTip={this.state.indexOfTip} />
+        <NavigationButton direction={"next"} update={this.nextTip} />
+      </div>
+    );
+  }
 }
 
-const TutorialImageContainer = () => {
+const TutorialImageContainer = (props) => {
   return (
     <div>
-      <p>ImageContainer</p>
+      <img src={tutorialImages[props.indexOfTip]} alt="" />
+      <p>{tutorialTips.tips[props.indexOfTip]}</p>
     </div>
   );
 };
 
-const NavigationButton = () => {
+const NavigationButton = (props) => {
   return (
     <div>
-      <button>button test</button>
+      <button onClick={props.update}>{props.direction}</button>
     </div>
   );
 }
